c7e77ff304ca3909216682cd39c854a688c41fdc
Sarati <friedchicken14821@gmail.com>
2023-07-15 00:54:16 -0500

    optimized 9 finished

--- rust/src/e9.rs
+++ rust/src/e9.rs
@@ -2,18 +2,14 @@ use std::time::Instant;
 
 pub fn e9() -> (i32, u128){
     let start_time = Instant::now();
-    for a in 0..1000{
-        for b in 0..1000{
-            for c in 0..1000{
-                if a >= b || b >= c {
-                    continue
-                }
-                if a*a + b*b == c*c{
-                    if a + b + c == 1000{
-            
-                        return (a * b * c, start_time.elapsed().as_nanos())
-                    }
-                }
+    for a in 0..1000 / 3 - 2{
+        for b in a + 1..=(1000 - a) / 2{
+            let c = 1000 - a - b;
+            if c <= b {
+                continue
+            }
+            if a*a + b*b == c*c{
+                return (a * b * c, start_time.elapsed().as_nanos())
             }
         }
     }
